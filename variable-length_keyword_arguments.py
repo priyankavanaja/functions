74fdaf52c63ef4a994eefdb3e976dcc251c2bb23
Priyanka <35795586+priyankavanaja@users.noreply.github.com>
2020-05-21 13:35:49 +0530

    Create variable-length_keyword_arguments.py

---
+++ variable-length_keyword_arguments.py
@@ -0,0 +1,19 @@
+# variable-length keyword arguments (**kwargs)
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Iterate over the key-value pairs of kwargs
+    for keys, values in kwargs.items():
+        # Print out the keys and values, separated by a colon ':'
+        print(keys + ": " + values)
+
+    print("\nEND REPORT")
+
+# First call to report_status()
+report_status(name="luke", affiliation="jedi", status="missing")
+
+# Second call to report_status()
+report_status(name="anakin", affiliation="sith lord", status="deceased")
